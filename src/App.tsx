65c2c26d8eedce38c30c44fff43d9eba008071c8
FelipeMondragon <luis.felipe.mondragon@proton.me>
2022-12-17 13:50:32 -0600

    listadoPeliculas, listado generico y bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import PeliculaIndividual from "./peliculas/PeliculaIndividual";
-import { pelicula } from "./peliculas/peliculas.model";
+import ListadoPeliculas from "./peliculas/ListadoPeliculas";
+// import PeliculaIndividual from "./peliculas/PeliculaIndividual";
+import { landingPageDTO } from "./peliculas/peliculas.model";
+import Button from "./utils/Button";
 
 function App() {
-  const peliculaPrueba: pelicula = {
-    id: 1,
-    titulo: "Spider-Man: Far from Home",
-    poster:
-      "https://blog.es.playstation.com/tachyon/sites/14/2022/06/adf0c6e3da060a9f9581c12eff047a48668fe616.jpg",
-  };
+  const [peliculas, setPeliculas] = useState<landingPageDTO>({});
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setPeliculas({
+        enCartelera: [
+          {
+            id: 1,
+            titulo: "Spider-Man: Far from Home",
+            poster:
+              "https://blog.es.playstation.com/tachyon/sites/14/2022/06/adf0c6e3da060a9f9581c12eff047a48668fe616.jpg",
+          },
+          {
+            id: 2,
+            titulo: "Moana",
+            poster:
+              "https://blog.es.playstation.com/tachyon/sites/14/2022/06/adf0c6e3da060a9f9581c12eff047a48668fe616.jpg",
+          },
+        ],
+        proximosEstrenos: [
+          {
+            id: 3,
+            titulo: "Soul",
+            poster:
+              "https://blog.es.playstation.com/tachyon/sites/14/2022/06/adf0c6e3da060a9f9581c12eff047a48668fe616.jpg",
+          },
+        ],
+      });
+    }, 500);
+
+    return () => clearTimeout(timerId);
+  });
+
   return (
     <>
-      <PeliculaIndividual pelicula={peliculaPrueba} />
+      <div className="container">
+        <Button>Mi componente botón</Button>
+
+        <h3>En Cartelera</h3>
+        <ListadoPeliculas peliculas={peliculas.enCartelera} />
+
+        <h3>Próximos estrenos</h3>
+        <ListadoPeliculas peliculas={peliculas.proximosEstrenos} />
+      </div>
     </>
   );
 }
