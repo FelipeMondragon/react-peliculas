48e7c502d69683adbe872424562f2f093682ef7d
FelipeMondragon <luis.felipe.mondragon@proton.me>
2023-01-04 13:46:51 -0600

    api url

--- src/generos/IndiceGeneros.tsx
+++ src/generos/IndiceGeneros.tsx
@@ -1,4 +1,19 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
+import { generoDTO } from "./genero.model";
+import { urlGeneros } from "../utils/endpoints";
+
 export default function IndiceGeneros() {
+  useEffect(() => {
+    axios
+      .get("https://localhost:7088/api/generos")
+      .then((respuesta: AxiosResponse<generoDTO[]>) => {
+        console.log(
+          "🚀 ~ file: IndiceGeneros.tsx:8 ~ axios.get ~ respuesta",
+          respuesta.data
+        );
+      });
+  }, []);
   return (
     <>
       <h3>Indice Géneros</h3>
